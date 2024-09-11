b1096a9f4d83b01fdaa9f67ef54ccdd44f6a862e
fioritog <166060237+fioritog@users.noreply.github.com>
2024-09-11 18:00:12 -0300

    Update Sala.cs

--- Cine/Cine/Modelos/Sala.cs
+++ Cine/Cine/Modelos/Sala.cs
@@ -1,31 +1,28 @@
-﻿
-
 namespace Cine.Modelos
 {
     public class Sala
     {
         public int Numero { get; private set; }
-        public List<Asientos> Asiento { get; private set; }
-        public Pelicula Pelicula { get; private set; }
+        public List<Asientos> Asiento { get; private set; } = new List<Asientos>();
+        public Pelicula? Pelicula { get; private set; } 
         public DateTime Horario { get; private set; }
         public Sala(int numero) 
         {
             Numero = numero;
         }
-        public Sala(int numero, List<Asientos> asiento, Pelicula pelicula, DateTime horario) 
+        public Sala(int numero, List<Asientos> asiento) 
         {
             Numero = numero;
             Asiento = asiento;
-            Pelicula = pelicula;
-            Horario = horario;
+
         }
         public void AgregarAsiento(Asientos asiento) 
         {
             Asiento.Add(asiento);
         }
-        public void AgregarAsiento(List<Asientos> asiento) 
+        public void AgregarAsiento(List<Asientos> asientos) 
         {
-            Asiento.AddRange(asiento);
+            Asiento.AddRange(asientos);
         }
         public void DefinirHorario(DateTime hora) 
         {
@@ -41,7 +38,7 @@ namespace Cine.Modelos
             /*DESOCUPAR LOS ASIENTOS DE LA SALA CUANDO TERMINA LA PELICULA*/
             foreach (var asiento in Asiento) 
             {
-                if (asiento.Ocupado == true) 
+                if (asiento.IsOcupado == true) 
                 {
                     asiento.CambiarEstado();
                 }
