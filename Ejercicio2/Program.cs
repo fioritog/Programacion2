2c491e2a5e66dca050c235c12cad8c93c05f9de3
fioritog <166060237+fioritog@users.noreply.github.com>
2024-10-02 17:19:56 -0300

    Add files via upload

---
+++ Ejercicio2/Program.cs
@@ -0,0 +1,38 @@
+﻿/*  Un editor de textos quiere implementar una funcionalidad para contar la frecuencia de las palabras en un artículo.
+
+El sistema debe:
+1. Leer una cadena de texto y separar las palabras.
+2. Contar cuántas veces aparece cada palabra.
+3. Mostrar la lista de palabras junto con su frecuencia.  */
+public static class Sistema 
+{
+    
+}
+class Program 
+{
+    static void Main()
+    {
+        Console.WriteLine("ingrese un texto:  ");
+        string texto = Console.ReadLine();
+
+        Dictionary<string, int> contadorPalabras = new Dictionary<string, int>();
+        string[] palabras = texto.Split(' ');
+        foreach (string palabra in palabras) 
+        {
+            if (contadorPalabras.ContainsKey(palabra))
+            {
+                contadorPalabras[palabra] += 1;
+            }
+            else
+            {
+                contadorPalabras.Add(palabra, 0);
+            }
+        }
+        Console.WriteLine($"\n");
+        Console.WriteLine($"Frecuencia de palabras: ");
+        foreach (var entrada in contadorPalabras) 
+        {
+            Console.WriteLine($"{entrada.Key}: {entrada.Value}");
+        }
+    }
+}
